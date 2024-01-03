f36fa077847bd0faf2f5135a0976db7bdfd120c3
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-01-03 12:16:31 +0100

    added basic JS!! e.g data type, funtion , string , boolean

--- basic/script.js
+++ basic/script.js
@@ -1,123 +1,107 @@
-//Variables 
+// Variables ****************************
 
-var myName = "Mehedi";
-console.log(myName); // Output : Mehedi
+// Declaring a variable with 'var'. This type of variable can be redeclared and updated.
+var myName = "Line";
+console.log(myName); // Output: Line
 
-//let age =31;
-//console.log(age); // Output : 31;
+// Declaring a variable with 'let'. This type can be updated but not redeclared within the same scope.
+let age = 31;
+console.log(age); // Output: 31
+console.log(typeof age); // Outputs the data type of age, which is 'number'.
 
-const  BRAND = "BMW";
-console.log(BRAND); // Output : BMW
+// Declaring a constant with 'const'. This type cannot be updated or redeclared.
+const MAKE = "BMW";
+console.log(MAKE); // Output: BMW
 
-//Data Types 
 
-//let greeting = "Hello, World";
-//console.log(typeof greeting);
+// Data Types ****************************
 
-//Boolean
+// Working with string data type
+let greeting = "Hello, World!";
+console.log(typeof greeting); // Outputs 'string', the type of the greeting variable.
+
+// Working with boolean data type
 let isHappy = true;
-console.log(isHappy);
-console.log(typeof isHappy);
+console.log(isHappy); // Outputs: true (a boolean value)
+console.log(typeof isHappy); // Outputs: 'boolean', the type of isHappy.
 
-//undefiend 
+// Undefined data type, declared but not assigned any value
 let someVar;
-console.log(typeof someVar)
+console.log(typeof someVar); // Outputs: 'undefined', as someVar has no assigned value.
 
-//Null
-//let emptyValue  = null;
-//console.log(emptyValue);
+// Null data type, explicitly nothing or 'empty'
+let emptyValue = null;
+console.log(typeof emptyValue); // Output: 'object' (historical reasons, should be 'null')
 
-//Object
+
+// Object Section ****************************
+
+// Creating an object. Objects store data in key-value pairs.
 let person = {
-    firstName: "Abdulla",
-    lastName: "Al Harun",
+    firstName: "Hesham",
+    lastName: "El Masry",
     age: 31
 };
-console.log(person);
-console.log(person.firstName);
-console.log(person.lastName);
-console.log(person.age);
 
+// Accessing object properties
+console.log(person); // Outputs the whole person object
+console.log(person.firstName); // Outputs person's first name
+console.log(person.lastName); // Outputs person's last name
+console.log(person.age); // Outputs person's age
+
+// Task: Create a car object with specified properties
 let car = {
-    make: "Volbo",
+    make: "Volvo",
     year: "1992",
-    condition: "used",
+    condition: "Used",
     price: 10000
 };
-console.log(car);
 
-// I have a Volbo car and it is 1992 and it is used and the price is 10000
-console.log("I have a "
-+car.make +" car and it is "
-+car.year +" and it is used and the price is 10000");
+// Accessing car object's properties using dot notation and bracket notation
+console.log(car.make); // Output: Volvo (dot notation)
+console.log(car["make"]); // Output: Volvo (bracket notation)
+console.log(car.condition); // Output: Used (dot notation)
 
-console.log(`I have a ${car.make} car and it is 1992 and it is used and the price is ${car.price}`);
+// Constructing a string using concatenation
+console.log("i have a " + car.make + " and it is " + car.year + " and it is used and the price is 10000");
+
+// Constructing a string using template literals
+console.log(`i have a ${car.make} and it is ${car.year} and it is used and the price is 10000`);
 
 
-//Array 
+// Array Section ****************************
+
+// Creating an array. Arrays store a collection of elements.
 let colors = ["Red", "Green", "Blue"];
 
+// Creating an array with mixed data types
 let random = [
-    55656, 
-    "green", 
-    {name: "Mehedi", age:31}
-    ["boy" , "girl", "whatever"]
+    55656, // Integer
+    "Green", // String
+    { name: "Hesh", age: 31}, // Object
+    ["boy", "girl", "whatever"] // Another Array
 ];
 
-console.log(random[0]);
-console.log(random[1]);
+// Accessing array elements by their index
+console.log(random[0]); // Outputs the first item in the array
+console.log(random[1]); // Outputs the second item in the array
 
-// Type Conversion
 
-let year= "1992"; //string 
+// Type Conversion Section  ****************************
 
-//from string to number 
+// Type Conversion: String to Number and Number to String
+
+// String to number conversion
+let year = "1992"; // String
+console.log(year); // Outputs the string '1992'
+
+// Converting the string 'year' to a number using parseInt()
 let convertedYear = parseInt(year);
-console.log(convertedYear);
+console.log(convertedYear); // Outputs the number 1992
 
-//Convert from number to string 
-let num = 123;
-console.log(num.toString());
+// Number to string conversion
+let num = 123; // Number
+console.log(num.toString()); // Converts and outputs '123' as a string
 
 let convertedNumber = num.toString();
-console.log(convertedNumber);
-
-
-//Task 1
-var city = "Oslo" ;
-console.log(city);
-
-let  age = 31;
-console.log(age);
-
-const birthYear = "1991";
-console.log(birthYear);
-
-//Task 2: Data Types 
-let greeting = ("Wellcome!");
-console.log(typeof greeting);
-
-let quantity  = 44;
-console.log(typeof quantity);
-
-let isJavaScriptFun = true;
-console.log(isJavaScriptFun);
-
-let thisIsUndefined;
-console.log(thisIsUndefined);
-
-let emptyValue  = null;
-console.log(emptyValue);
-
-//Task 3: Type Conversion
-let numString = "25";
-
-let Num = parseInt (numString);
-console.log(Num);
-
-let num2 = 50;
-let numString2 = num2.toString();
-console.log(num2);
-
-let sum = num + num2;
-console.log(sum);
+console.log(convertedNumber); // Outputs the string '123'
