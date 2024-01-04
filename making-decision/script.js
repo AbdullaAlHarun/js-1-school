b388ffb95ebd8236d43f3a0a872ee9539ca15915
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-01-04 13:13:41 +0100

    added Switch case!!

--- making-decision/script.js
+++ making-decision/script.js
@@ -29,7 +29,7 @@ else {
 let result = "A"
 
 if (result === "A"  ) {
-    console.log("Very Good" + " You got a core of: " + result);
+    console.log("Very Good" + " You got a core \n of: " + result);
 } else if ( result === "B") {
     console.log(" Good");
 } else if ( result === "C") {
@@ -38,4 +38,56 @@ if (result === "A"  ) {
     console.log("Start to think");
 } else {
     console.log("you fail badly")
+}
+
+// task if else with number 
+let myGrade = 50;
+
+if (myGrade >= 90) {
+    console.log("A");
+} else if (myGrade >=70) {
+    console.log("B")
+} else {
+    console.log("You fail");
+}
+
+// 2. Switch case 
+let color = "grehjkhkhen";
+
+switch(color) {
+    case "red":
+        console.log("Stop!");
+        break;
+    case "yellow":
+        console.log("Get ready!!");
+        break;
+    case "green":
+        console.log("Go!");
+        break;
+    default:
+        console.log("No Traffic lights , murder people!");
+}
+
+// Switch case class Task 
+
+let grade = "A";
+
+switch(grade) {
+case "A": 
+    console.log("Outstanding!");
+    break;
+case "B":
+    console.log("Very Good!");
+    break;
+case "C":
+    console.log("Good Effort!");
+    break;
+case "D":
+    console.log("Made improvment!");
+    break;
+case "F":
+    console.log("Failed!");
+    break;
+default:
+    console.log("Invalid Grade");
 }
