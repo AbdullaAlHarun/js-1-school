11af8010bdfb9cc18628405d35ef53c2b51650af
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-01-04 12:51:26 +0100

    added if....else , else .... if ! else

---
+++ making-decision/script.js
@@ -0,0 +1,41 @@
+// Making Decision 
+// If and else statements! 
+
+let temp = 28;
+
+if(temp > 30){
+    console.log("It is very hot!!");
+} else if (temp > 20){
+    console.log("fail")
+} else {
+    console.log("The weather is very cold!")
+}
+
+// Condition!! task 
+let score = 60;
+
+if (score => 50) {
+    console.log("pass");
+} 
+else {
+    console.log("fail");
+}
+
+// A = very good 
+// B =Good 
+// C = Average 
+// D = start to think 
+
+let result = "A"
+
+if (result === "A"  ) {
+    console.log("Very Good" + " You got a core of: " + result);
+} else if ( result === "B") {
+    console.log(" Good");
+} else if ( result === "C") {
+    console.log(" Avarage");
+} else if ( result === "D") {
+    console.log("Start to think");
+} else {
+    console.log("you fail badly")
+}
