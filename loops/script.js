983c32380496ce0c33fa5c8a7f40817662039d84
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-01-05 13:00:13 +0100

    added two for .... loops example !!!

--- loops/script.js
+++ loops/script.js
@@ -1,6 +1,50 @@
 //Looping (for loop)
-
-for (let i = 1; i <= 5; i++) {
+// loops is fore repating! it's very important! 
+//for (initialization;condition;increment) {}
+//initialization is a variable, people called it i 
+for (let i = 0; i < 5; i++) {
     console.log(i);
 }
+
+for (let i = 1; i < 11; i++) {
+    console.log(i);
+}
+
+const cars = ["Tesla", "BMW", "Volvo", "VolksWagen"];
+console.log(cars.length);
+
+const totalNumberOfCars = cars.length;
+
+for (let i = 0; i <totalNumberOfCars; i++) {
+    console.log(cars[i])
+}
+
+
+const fruits = ["melon", "strawberry", "mango", "banna", "apple"];
+console.log(fruits.length);
+
+const totalNumberOfFruits = fruits.length;
+
+for (let i = 0; i < totalNumberOfFruits; i++) {
+    console.log(fruits[i])
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
