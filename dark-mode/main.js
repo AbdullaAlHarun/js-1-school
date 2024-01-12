307ada30216099eccdaa3b6ce941c18c4cceaa6d
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-01-12 15:38:07 +0100

    added  final js!

--- dark-mode/main.js
+++ dark-mode/main.js
@@ -1,28 +1,38 @@
-// addEventListener 
-// onClick 
+// addEventListener
+// onclick
+
+// 1. select the body
 
 const body = document.querySelector("body");
+const toggleBtn = document.querySelector(".toggle-btn");
+let isDarkMode = false; // my toggle
 
-const darkModeBtn = document.querySelector(".mode-btn");
-console.log(body);
-console.log(darkModeBtn);
-
-let isDarkMode = false;
-console.log("I click on dark mode btn before: ", isDarkMode);
-
-function toggleMode () {
-    isDarkMode = ! isDarkMode;
-
-    if (isDarkMode){
-        body.style.backgroundColor = "#0A0A0A";
-     
+function init() {
+    const currentMode = window.localStorage.getItem("mode");
+    if (currentMode) {
+        console.log("currentMode: ", currentMode);
+        if (currentMode === "dark") {
+            isDarkMode = true;
+            body.style.backgroundColor = "darkblue";
+        } else { // light
+            isDarkMode = false;
+            body.style.backgroundColor = "white";
+        }
     } else {
-        body.style.backgroundColor = "lightgreen";
+        console.log("no mode yet");
     }
-
 }
 
-darkModeBtn.addEventListener("click", toggleMode);
+init();
 
-
-
+toggleBtn.addEventListener("click", function () {
+    isDarkMode = !isDarkMode;
+    console.log("isDarkMode: ", isDarkMode);
+    if (isDarkMode) { // true
+        body.style.backgroundColor = "darkblue";
+        window.localStorage.setItem("mode", "dark");
+    } else { // false
+        body.style.backgroundColor = "white";
+        window.localStorage.setItem("mode", "light");
+    }
+});
