9d0443c73b71cb437436e712d2da40c41b5e172e
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-01-16 13:59:00 +0100

    added a function of faviroute projects!

---
+++ building-a-favourites-page/data/products.js
@@ -0,0 +1,25 @@
+export default [
+    {
+        id: 1,
+        name: "iphone 15",
+        pric: 10000
+    },
+    {
+        id: 2,
+        name: "iphone 14",
+        pric: 10000
+    },
+   
+    {
+        id: 3,
+        name: "iphone 15",
+        pric: 10000
+    },
+   
+    {
+        id: 2,
+        name: "iphone 15",
+        pric: 10000
+    },
+
+];
