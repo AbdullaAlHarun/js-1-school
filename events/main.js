03d83a805bc466a9cff6a9cd5303bfc161d1170d
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-01-18 14:05:12 +0100

    added html design!

---
+++ events/main.js
@@ -0,0 +1,15 @@
+
+
+saySomeThingFunction ();
+
+function saySomethingFunction (){
+    console.log("hello Mehedi")
+}
+
+const button = document.querySelector(".btn");
+console.log(button);
+
+//way 1 
+button.addEventListener("click",function(){
+    console.log("you clicked me OMG!!");
+})
