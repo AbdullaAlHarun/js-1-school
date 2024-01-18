3598680bc6422b04ca05d4547adbb1da045c3579
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-01-18 14:26:34 +0100

    addEventListener added!

--- events/main.js
+++ events/main.js
@@ -1,7 +1,5 @@
 
 
-saySomeThingFunction ();
-
 function saySomethingFunction (){
     console.log("hello Mehedi")
 }
@@ -12,4 +10,37 @@ console.log(button);
 //way 1 
 button.addEventListener("click",function(){
     console.log("you clicked me OMG!!");
-})
+});
+
+
+button.addEventListener("click: ", handlClick);
+function handlClick() {
+    console.log("you clicked on me OMG OMG!!");
+}
+
+
+//way2  
+
+button.onclick = function (){
+    console.log("OMG way2 clicked");
+}
+
+button.onclick = function (event){
+    console.log(event.target.value);
+    console.log(this.value);
+}
+
+// Q3 key press from keybord 
+//onKeyDown 
+// onKeyUp
+//onKeyPress 
+
+const myFirstNameInput = document.querySelector("#firsName");
+
+const handelKeyPress = function() {
+    console.log("a key is pressed");
+    console,log(this.value);
+}
+
+myFirstNameInput.addEventListener("keybord", handelKeyPress);
+
