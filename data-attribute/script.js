9369fc9c172f11b5ea1680d0d1076d0b636c4b01
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-01-25 13:23:45 +0100

    added a data attribute example!

---
+++ data-attribute/script.js
@@ -0,0 +1,16 @@
+const article = document.querySelector("#post");
+console.dir(article);
+console.log(article.dataset);
+console.log(article.author);
+
+const postDetails = document.querySelector("#post-details");
+console.log(postDetails);
+
+//alert(postDetails);
+//document.getElementById('#post-details');
+
+postDetails.addEventListener("Click",function(){
+    alert(`the book author is:" ${article.dataset.author} and the catagory of the book is
+    ${article.dataset.catagory}, Tusen takk <3 !!
+    `)
+});
